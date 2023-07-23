3f47aa7aca8bd97a4fc5a17c7833c6cedffdea15
CarlosDRepo <dodc881207@gmail.com>
2023-07-23 12:17:28 -0500

    Solucion a desafio de identifica la operacion

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function solution(){
 }
 
 solution();
+
+function factor(){
+    let results = [27, 36, 45, 54, 63, 72];
+
+    let division = results[0] / 3;
+
+    for (let i = 1; i < results.length; i++) {
+        if (results[i] % division != 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(factor()); // true o false dependiendo de los datos
+
